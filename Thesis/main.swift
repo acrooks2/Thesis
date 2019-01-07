ae8dab033ed0822ba61fc7c1db6a087252a1686c
Charlie <charlie.a.roe@gmail.com>
2019-01-07 11:18:18 -0500

    just building, not running yet

--- Thesis/main.swift
+++ Thesis/main.swift
@@ -19,29 +19,48 @@ struct Order {
     var price: Int
 }
 
+struct Trade {
+    var restingTraderID: Int
+    var restingOrderID: Int
+    var restingTimeStamp: Int
+    var incomingTraderID: Int
+    var incomingOrderID: Int
+    var incomingTimeStamp: Int
+    var tradePrice: Int
+    var tradeQuantity: Int
+    var side: Int
+}
+
 struct BidBook {
     //always sorted array of bid prices
     var prices: SortedArray<Int>
-    //orders is a dictionary with prices as keys and a list of orders at each value
+    //orders is a dictionary with exIDs as keys and the associated order as values
     var orders: [Int:Order]
     //the number of orders at each price
     var numOrders: [Int:Int]
     //the quantity at each price
     var priceSize: [Int:Int]
-    var exIDs: [Int:[Int]]
+    // dictionary with prices as keys and lists of order ids as values
+    var orderIDs: [Int:[Int]]
     
 }
 
 struct AskBook {
     //always sorted array of ask prices
     var prices: SortedArray<Int>
-    //orders is a dictionary with prices as keys and a list of orders at each value
+    //orders is a dictionary with exIDs as keys and the associated order as values
     var orders: [Int:Order]
     //the number of orders at each price
     var numOrders: [Int:Int]
     //the quantity at each price
     var priceSize: [Int:Int]
-    var exIDs: [Int:[Int]]
+    // dictionary with prices as keys and lists of order ids as values
+    var orderIDs: [Int:[Int]]
+}
+
+struct TradeBook {
+    // A dictionary to store all trades with trade ID as key and trades as values
+    var trades: [Int:Trade]
 }
 
 class OrderBook {
@@ -50,13 +69,25 @@ class OrderBook {
     var askBook: AskBook
     var orderIndex: Int
     var exIndex: Int
+    var traded: Bool
+    var confirmTradeCollector: [Order]
+    var tradeBook: TradeBook
+    var tradeIndex: Int
+    var lookUp: [Int:[Int:Order]]
     
-    init(bidBook: BidBook, askBook: AskBook) {
+    init(bidbook: BidBook, askbook: AskBook, tradebook: TradeBook) {
         self.orderHistory = [:]
-        self.bidBook = bidBook
-        self.askBook = askBook
+        self.bidBook = bidbook
+        self.askBook = askbook
+        // Order index is simply to identify orders in sequence in order history
         self.orderIndex = 0
+        // ex index is for identifying limit orders will hit in process order
         self.exIndex = 0
+        self.traded = false
+        self.confirmTradeCollector = []
+        self.tradeBook = tradebook
+        self.tradeIndex = 0
+        self.lookUp = [:]
     }
     
     func addOrderToHistory(order: Order) {
@@ -66,42 +97,102 @@ class OrderBook {
     
     func addOrderToBook(order: Order) {
         exIndex += 1
+        // Add an order to the buy side
         if order.side == 1 {
             if bidBook.prices.contains(order.price) {
-                bidBook.numOrders[order.price] += 1
-                bidBook.priceSize[order.price] += order.quantity
-                bidBook.exIDs[order.price]?.append(exIndex)
+                bidBook.numOrders[order.price]! += 1
+                bidBook.priceSize[order.price]! += order.quantity
+                bidBook.orderIDs[order.price]!.append(exIndex)
                 bidBook.orders[exIndex] = order
             }
             else {
                 bidBook.prices.insert(order.price)
                 bidBook.numOrders[order.price] = 1
                 bidBook.priceSize[order.price] = order.quantity
-                bidBook.exIDs[order.price]?.append(exIndex)
+                bidBook.orderIDs[order.price]!.append(exIndex)
                 bidBook.orders[exIndex] = order
         
             }
         }
+        // Add an order to the sell side
         else {
             if askBook.prices.contains(order.price) {
-                askBook.numOrders[order.price] += 1
-                askBook.priceSize[order.price] += order.quantity
-                askBook.exIDs[order.price]?.append(exIndex)
+                askBook.numOrders[order.price]! += 1
+                askBook.priceSize[order.price]! += order.quantity
+                askBook.orderIDs[order.price]?.append(exIndex)
                 askBook.orders[exIndex] = order
             }
             else {
                 askBook.prices.insert(order.price)
                 askBook.numOrders[order.price] = 1
                 askBook.priceSize[order.price] = order.quantity
-                askBook.exIDs[order.price]?.append(exIndex)
+                askBook.orderIDs[order.price]?.append(exIndex)
                 askBook.orders[exIndex] = order
+            }
+        }
+    }
+    
+    func confirmTrade(order: Order) {
+        confirmTradeCollector.append(order)
+    }
+    
+    func addTradeToBook(trade: Trade) {
+        tradeBook.trades[tradeIndex] = trade
+    }
+    
+    func removeOrder(order: Order) {
+        if order.side == 1 {
+            bidBook.numOrders[order.price]! -= 1
+            bidBook.priceSize[order.price]! -= order.quantity
+            bidBook.orderIDs[order.price]!.removeLast()
+            if bidBook.numOrders[order.price]! == 0 {
+                bidBook.prices.remove(order.price)
+            }
+        }
+    }
+    
+    func matchTrade(order: Order) {
+        traded = true
+        confirmTradeCollector.removeAll()
+        // If order side is "buy"
+        var remainder = order.quantity
+        if order.side == 1 {
+            while remainder > 0 {
+                var price = askBook.prices[0]
+                if order.price >= price {
+                    var orderID = askBook.orderIDs[order.price]![0]
+                    var bookOrder = askBook.orders[orderID]
+                    if remainder >= bookOrder!.quantity {
+                        confirmTrade(order: order)
+                        var trade = Trade(restingTraderID: (bookOrder?.traderID)!, restingOrderID: (bookOrder?.orderID)!, restingTimeStamp: (bookOrder?.timeStamp)!, incomingTraderID: order.traderID, incomingOrderID: order.orderID, incomingTimeStamp: order.timeStamp, tradePrice: (bookOrder?.price)!, tradeQuantity: (bookOrder?.quantity)!, side: order.side)
+                        addTradeToBook(trade: trade)
+                        //remove order func
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    func processOrder(order: Order) {
+        traded = false
+        addOrderToHistory(order: order)
+        // If order type is "add"
+        if order.type == 1 {
+            // If order side is "buy"
+            if order.side == 1 {
+                // If price is greater than or equal to lowest ask book price (i.e. is marketable)
+                if order.price >= askBook.prices[0] {
+                    // match trade func
+                }
             }
         }
     }
 }
 
-var bb = BidBook(prices: SortedArray<Int>(), orders: [:], numOrders: [:], priceSize: [:], exIDs: [:])
-var ab = AskBook(prices: SortedArray<Int>(), orders: [:], numOrders: [:], priceSize: [:], exIDs: [:])
-var ob = OrderBook(bidBook: bb, askBook: ab)
+var bb = BidBook(prices: SortedArray<Int>(), orders: [:], numOrders: [:], priceSize: [:], orderIDs: [:])
+var ab = AskBook(prices: SortedArray<Int>(), orders: [:], numOrders: [:], priceSize: [:], orderIDs: [:])
+var tb = TradeBook()
+var ob = OrderBook(bidbook: bb, askbook: ab, tradebook: tb)
 
 print(ob.askBook)
